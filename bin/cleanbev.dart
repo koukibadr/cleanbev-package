6dd00123a4ae33648b57164fb4c6e4e37bb91440
Badreddine Kouki <badreddinekouki1998@gmail.com>
2026-02-17 11:28:06 +0300

    feat: add help flag argument

--- bin/cleanbev.dart
+++ bin/cleanbev.dart
@@ -2,6 +2,10 @@ import 'package:cleanbev/cleanbev.dart';
 
 void main(List<String> arguments) {
   final CleanbevArgResults config = CleanbevArgTransform().parse(arguments);
+  if (config.showHelp) {
+    print(CleanbevArgTransform().usage);
+    return;
+  }
   final AssetsListParser parser = AssetsListParser(
     assetsPath: config.assetsPath,
   );
--- lib/src/cleanbev_arg_transform.dart
+++ lib/src/cleanbev_arg_transform.dart
@@ -2,27 +2,55 @@ import 'package:args/args.dart';
 
 class CleanbevArgResults {
   final String assetsPath;
+  final bool acceptAll;
+  final bool showHelp;
 
-  CleanbevArgResults({required this.assetsPath});
+  CleanbevArgResults(
+      {required this.assetsPath,
+      required this.acceptAll,
+      required this.showHelp});
 }
 
 /// Transforms command-line arguments into a [CleanbevArgResults] instance.
 class CleanbevArgTransform {
   static const _assetPathKey = 'assets-path';
 
+  static const _acceptAllKey = 'accept-all';
+
+  static const _helpKey = 'help';
+
   final _parser = ArgParser()
+    ..addFlag(
+      _helpKey,
+      abbr: 'h',
+      help: 'Print this usage information.',
+      negatable: false,
+    )
+    ..addFlag(
+      _acceptAllKey,
+      help:
+          'Accept all assets without confirmation, including those that are not used in the project. Use with caution.',
+      defaultsTo: false,
+    )
     ..addOption(
       _assetPathKey,
-      help: 'The path to the assets directory.',
+      help:
+          'The path to the assets directory. Defaults to "assets". If the directory does not exist, an exception will be thrown.',
       mandatory: false,
       defaultsTo: 'assets',
     );
 
+  String get usage => _parser.usage;
+
   CleanbevArgResults parse(List<String> args) {
     final rawResults = _parser.parse(args);
     if (rawResults[_assetPathKey] == null) {
-      throw FormatException('The --assets-path option is required.');
+      throw FormatException('The --assets-path argument is required.');
     }
-    return CleanbevArgResults(assetsPath: rawResults[_assetPathKey] as String);
+    return CleanbevArgResults(
+      assetsPath: rawResults[_assetPathKey] as String,
+      acceptAll: rawResults[_acceptAllKey] as bool,
+      showHelp: rawResults[_helpKey] as bool,
+    );
   }
 }
